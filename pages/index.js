587c6d5a7bca2afaf0af79a1a3aadecf64c7955b
phthom <gitops@cloudnativetoolkit.dev>
2021-09-17 16:34:19 +0200

    version 12

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API by Phil.</p>
+            <p>Find in-depth information about Next.js features and API by Phil 2.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
